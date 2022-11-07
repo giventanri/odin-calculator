acdb1eafd242e7160bb80d92bc4d6e5e64b1e158
giventanri <given.tanri@my.wheaton.edu>
2022-11-07 12:43:18 -0600

    Create +-*/ functions

---
+++ script.js
@@ -0,0 +1,6 @@
+function add(a,b) {return a + b;}
+function substract(a,b) {return a - b;}
+function multiply(a,b) {return a * b;}
+function divide(a,b) {return a / b;}
+
+function operate(operator, a, b) {return operator(a,b);}
